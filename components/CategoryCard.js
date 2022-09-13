9a7df04dbd0637e8588c6da695c4ee8d627112a3
Brunno Lorenzoni <brunno.lorenzoni@unicred.com.br>
2022-09-13 13:54:15 -0300

    Default Nextjs Link component

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 const CategoryCard = ({ category }) => {
   const { title, slug, icon } = category.fields
   return (
     <div className="card bg-white rounded overflow-hidden shadow-lg w-full mb-6">
-      <a href={`/categories/${slug}`}>
+      <Link href={`/categories/${slug}`}>
+      <a>
         <picture className="block h-64 overflow-hidden">
           <source src={`https:${icon.fields.file.url}`} type={icon.fields.file.contentType} />
           <img className="object-cover m-auto object-center h-full" src={`https:${icon.fields.file.url}`} alt={icon.fields.description} />
@@ -11,6 +14,7 @@ const CategoryCard = ({ category }) => {
           <h2 className="font-bold text-xl mb-2">{title}</h2>
         </div>
       </a>
+      </Link>
     </div>
   );
 };
--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import TagLabel from "./TagLabel";
 
 const ProductCard = ({ product }) => {
@@ -5,18 +6,20 @@ const ProductCard = ({ product }) => {
   
   return (
     <div className="card bg-white rounded overflow-hidden shadow-lg w-full mb-6">
-      <a href={`/product/${slug}`}>
-        <picture className="block h-64 overflow-hidden">
-          <source src={`https:${image[0].fields.file.url}`} type={image[0].fields.file.contentType} />
-          <img className="object-cover m-auto object-center h-full" src={`https:${image[0].fields.file.url}`} alt={image[0].fields.description} />
-        </picture>
-        <div className="px-6 py-4">
-          <h2 className="font-bold text-xl mb-2">{productName}</h2>
-          <p className="text-base">
-            {productDescription.length > 50 ? `${productDescription.substring(0, 50).trim()}...` : productDescription}
-          </p>
-        </div>
-      </a>
+      <Link href={`/product/${slug}`}>
+        <a>
+          <picture className="block h-64 overflow-hidden">
+            <source src={`https:${image[0].fields.file.url}`} type={image[0].fields.file.contentType} />
+            <img className="object-cover m-auto object-center h-full" src={`https:${image[0].fields.file.url}`} alt={image[0].fields.description} />
+          </picture>
+          <div className="px-6 py-4">
+            <h2 className="font-bold text-xl mb-2">{productName}</h2>
+            <p className="text-base">
+              {productDescription.length > 50 ? `${productDescription.substring(0, 50).trim()}...` : productDescription}
+            </p>
+          </div>
+        </a>
+      </Link>
       <div className="px-6 pt-4 pb-2">
         {tags.map(tag => <TagLabel key={tag}>#{tag}</TagLabel>)}
       </div>
