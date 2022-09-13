3d5df584a3b7503ca286083b22c7973e8a864771
Brunno Lorenzoni <brunno.lorenzoni@unicred.com.br>
2022-09-13 12:53:48 -0300

    categories page + category card

---
+++ components/CategoryCard.js
@@ -0,0 +1,18 @@
+const CategoryCard = ({ category }) => {
+  const { title, slug, icon } = category.fields
+  return (
+    <div className="card bg-white rounded overflow-hidden shadow-lg w-full mb-6">
+      <a href={`/categories/${slug}`}>
+        <picture className="block h-64 overflow-hidden">
+          <source src={`https:${icon.fields.file.url}`} type={icon.fields.file.contentType} />
+          <img className="object-cover m-auto object-center h-full" src={`https:${icon.fields.file.url}`} alt={icon.fields.description} />
+        </picture>
+        <div className="px-6 py-4">
+          <h2 className="font-bold text-xl mb-2">{title}</h2>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default CategoryCard;
--- pages/categories/index.js
+++ pages/categories/index.js
@@ -1,19 +1,44 @@
+import contentful from 'lib/contentful'
+import CategoryCard from 'components/CategoryCard'
+
 export default function CaegoriesPage({ categories }) {
+  console.log(categories)
+
   return (
-    <div>
-      <h1>Categories</h1>
-      <h2>here will have a list</h2>    
-    </div>
+    <>
+      <header>
+        <h1>Categories</h1>
+      </header>
+
+      <main>
+        <section id="categories-section" className='grid'>
+          {categories.map(category => (
+            <CategoryCard key={category.sys.id} category={category}></CategoryCard>
+          ))}
+        </section>
+      </main>
+    </>
   )
 }
 
 export async function getServerSideProps(context) {
 
-  
+  const categories = await contentful.getEntries({
+    content_type: 'category',
+  })
+
+  if(!categories || !categories.items.length) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/',
+      },
+    };
+  }
 
   return {
     props: {
-      categories: []
-    }, // will be passed to the page component as props
+      categories: categories.items
+    },
   }
 }
