b5d4b855dcc57c1f2dc9b955ad6eaa726552b743
Brunno Lorenzoni <brunno.lorenzoni@unicred.com.br>
2022-09-14 14:24:38 -0300

    header structure

--- components/Header.js
+++ components/Header.js
@@ -5,26 +5,37 @@ import { HiShoppingBag } from 'react-icons/hi'
 
 const Header = () => {
   return (
-    <header className="h-14 bg-gray-900 text-white	">
-      <Link href='/'>
-        <a>
-          <HiShoppingBag/>
-          <h1>Beautiful Shop</h1>
-        </a>
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            Home
-          </li>
-          <li>
-            About
-          </li>
-          <li>
-            Categories
-          </li>
-        </ul>
+    <header className="p-2 min-h-[60px] bg-gray-900 text-white flex flex-row justify-between flex-wrap">
+      
+      <div className="flex flex-row justify-center items-center flex-wrap">
+        <Link href='/'>
+          <a>
+            <div className="flex justify-center items-center">
+              <HiShoppingBag className="inline-block mr-1"/>
+              <h1 className="inline-block">Beautiful Shop</h1>
+            </div>
+          </a>
+        </Link>
+      </div>
+
+      <nav className="flex flex-row justify-center items-center flex-wrap">
+        <div className="p-2 m-2">
+          <Link href='/'>
+            <a>Home</a>
+          </Link>
+        </div>
+        <div className="p-2 m-2">
+          <Link href='/about'>
+            <a>About</a>
+          </Link>
+        </div>
+        <div className="p-2 m-2">
+          <Link href='/categories'>
+          <a>Categories</a>
+          </Link>
+        </div>
       </nav>
+      
     </header>
   );
 };
