42417f5b8ce57e62ab24ad44f7118a958fee8591
Brunno Lorenzoni <brunno.lorenzoni@unicred.com.br>
2022-09-16 07:52:09 -0300

    header on mobile + 404 page

--- components/Header/Dropdown.js
+++ components/Header/Dropdown.js
@@ -2,19 +2,19 @@ import Link from "next/link";
 
 const Dropdown = ({ dropdown  }) => {
   return (
-    <ul className={`p-2 m-2 absolute opacity-90 right-0 z-10 bg-gray-700 shadow-xl rounded ${dropdown ? 'block' : 'hidden'}`}>
+    <ul className={`p-2 m-2 relative md:absolute opacity-90 right-0 z-10 bg-gray-700 shadow-xl rounded ${dropdown ? 'block' : 'hidden'}`}>
       <li>
-        <Link href='/categories/home-kitchen'>
+        <Link href='/categories/home-kitchen' replace>
           <a className="block p-1 my-1 whitespace-nowrap text-sm opacity-100">Home & Kitchen</a>
         </Link>
       </li>
       <li>
-        <Link href='/categories/smartphones'>
+        <Link href='/categories/smartphones' replace>
           <a className="block px-1 my-1 whitespace-nowrap text-sm opacity-100">Smartphones</a>
         </Link>
       </li>
       <li>
-        <Link href='/categories/toys'>
+        <Link href='/categories/toys' replace>
           <a className="block p-1 my-1 whitespace-nowrap text-sm opacity-100">Toys</a>
         </Link>
       </li>
--- components/Header/Navbar.js
+++ components/Header/Navbar.js
@@ -1,40 +1,42 @@
 import Link from "next/link";
 import Dropdown from "components/Header/Dropdown";
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { IoClose, IoChevronBackCircleOutline } from 'react-icons/io5'
 import { useState } from 'react';
 
 const Navbar = () => {
 
   const [dropdown, setDropdown] = useState(false);
-  const [menu, setMenu] = useState(false);
+  const [toogleMenu, setToogleMenu] = useState(false);
 
-  const handlerMouseEnterMenu = () => setDropdown(true);
-  const handlerMouseLeaveMenu = () => setDropdown(false);
-
-  const handleOpenMenu = () => {
-    setMenu(!menu)
-  }
+  const toggleDropdown = () => setDropdown(prev => !prev);
+  const toggleMenuHamburguer = () => setToogleMenu(prev => !prev);
 
   return (
-    <nav className="flex flex-row justify-between items-center flex-wrap">
-      <button className="block md:hidden" onClick={handleOpenMenu}> 
+    <nav className="flex flex-row justify-between items-center flex-wrap ">
+      <button className="block md:hidden" onClick={toggleMenuHamburguer}> 
         <GiHamburgerMenu className="text-3xl" />
       </button>
 
-      <ul className="hidden md:flex flex-row justify-center items-center flex-wrap">
-        <li className="p-2 m-2">
-          <Link href='/'>
+      <ul className={`${toogleMenu ? 'flex fixed z-20 top-0 left-0 w-full min-h-screen	bg-gray-900' : 'hidden'} md:flex md:relative md:min-h-min flex-row content-center justify-center items-center flex-wrap`} >
+        
+        <button className="block md:hidden fixed top-10 right-10" onClick={toggleMenuHamburguer}> 
+          <IoClose className="text-3xl" />
+        </button>
+        
+        <li className="block md:inline-block w-full md:w-min text-center p-2 m-2">
+          <Link href='/' replace>
             <a>Home</a>
           </Link>
         </li>
-        <li className="p-2 m-2 ">
-          <Link href='/about'>
+        <li className="block md:inline-block w-full md:w-min text-center p-2 m-2">
+          <Link href='/about' replace>
             <a>About</a>
           </Link>
         </li>
-        <li className="p-2 m-2"
-          onMouseEnter={handlerMouseEnterMenu}
-          onMouseLeave={handlerMouseLeaveMenu}
+        <li className="block md:inline-block w-full md:w-min text-center p-2 m-2"
+          onMouseEnter={toggleDropdown}
+          onMouseLeave={toggleDropdown}
         >
           <button
             aria-expanded={dropdown ? "true" : "false"}
@@ -42,6 +44,11 @@ const Navbar = () => {
           >Categories</button>
           <Dropdown dropdown={dropdown} />
         </li>
+
+        <button className="block md:hidden mt-20" onClick={toggleMenuHamburguer}> 
+          <IoChevronBackCircleOutline className="text-3xl" />
+        </button>
+
       </ul>
     </nav>
   );
